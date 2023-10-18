b5da17e41ec12a34fb790aeef81e27d4253bb602
SunSual <mrtaktplay@gmail.com>
2023-10-19 00:40:11 +0300

    1.3.0

---
+++ Helpers/TcpHelper.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AnimeList.Helpers
+{
+    public class TcpHelper
+    {
+        private TcpListener listener;
+        private TcpClient client;
+        public string Code = "";
+        public string FirstIpPart = "";
+        public delegate void Connected(TcpClient client);
+        public event Connected OnConnected;
+        public event Connected OnAccepted;
+        public delegate void Error(string message);
+        public event Error OnError;
+
+        public TcpHelper()
+        {
+
+
+        }
+
+        public void Start()
+        {
+            try
+            {
+                var resolved = Dns.GetHostEntry(Dns.GetHostName());
+                var found = resolved.AddressList.FirstOrDefault(x => x.AddressFamily == AddressFamily.InterNetwork);
+
+
+                var ip = found.ToString();
+
+                Console.WriteLine(ip);
+
+                var parts = ip.Split('.');
+                var gateway = parts[parts.Length - 1];
+
+                Console.WriteLine(gateway);
+                FirstIpPart = parts[0] + "." + parts[1] + "." + parts[2] + ".";
+
+                if (Convert.ToInt32(gateway) == 1)
+                {
+                    Code = "Нет сети";
+                    OnError?.Invoke("Нет подключения к сети.");
+                    return;
+                }
+
+
+
+                listener = new TcpListener(System.Net.IPAddress.Any, 0);
+                listener.Start();
+                var port = ((IPEndPoint)listener.LocalEndpoint).Port;
+
+
+
+
+                Code = ConvertToBase62(Convert.ToInt32(gateway)) + "-" + ConvertToBase62(Convert.ToInt32(port));
+            } catch (Exception ex)
+            {
+                Code = "Нет сети";
+            }
+        }
+
+        public async Task StartWaiting()
+        {
+            try
+            {
+                var client = await listener.AcceptTcpClientAsync();
+                OnAccepted?.Invoke(client);
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        public async Task<Models.AnimeList> ReadIncomingAsync()
+        {
+            try
+            {
+                var stream = client.GetStream();
+
+                StreamReader sr = new StreamReader(stream);
+                if (client.Connected)
+                {
+                    var reading = await sr.ReadLineAsync();
+                    var gotList = JsonSerializer.Deserialize<Models.AnimeList>(reading, new JsonSerializerOptions()
+                    {
+                        IncludeFields = true,
+                        PropertyNameCaseInsensitive = true
+                    });
+                    return gotList;
+                }
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return null;
+        }
+
+        public async Task SendAnimeList(TcpClient connectedClient, Models.AnimeList list)
+        {
+            try 
+            {
+                var stream = connectedClient.GetStream();
+                var sw = new StreamWriter(stream);
+                sw.WriteLine(JsonSerializer.Serialize(list));
+                await sw.FlushAsync();
+            } 
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}");
+            }
+        }
+
+        public string ReturnCode()
+        {
+            return Code;
+        }
+
+        public void Dispose()
+        {
+            try
+            {
+                if(listener != null)
+                    listener.Stop();
+                if (client != null)
+                {
+                    client.Close();
+                    client.Dispose();
+                }
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public static long ConvertToDecimal(string base62Number)
+        {
+            const string digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            int baseCount = 62;
+            long result = 0;
+
+            for (int i = 0; i < base62Number.Length; i++)
+            {
+                char digit = base62Number[i];
+                int value = digits.IndexOf(digit);
+                result = result * baseCount + value;
+            }
+
+            return result;
+        }
+
+        public static string ConvertToBase62(long decimalNumber)
+        {
+            const string digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            int baseCount = 62;
+            StringBuilder result = new StringBuilder();
+
+            while (decimalNumber > 0)
+            {
+                int remainder = (int)(decimalNumber % baseCount);
+                result.Insert(0, digits[remainder]);
+                decimalNumber /= baseCount;
+            }
+
+            return result.ToString();
+        }
+
+        public async Task Connect(string codeEntered)
+        {
+            client = new TcpClient(AddressFamily.InterNetwork);
+            
+            var split = codeEntered.Split('-');
+            
+            var gateway = ConvertToDecimal(split[0]);
+            var port = ConvertToDecimal(split[1]);
+
+            Console.WriteLine(gateway);
+            Console.WriteLine(port);
+
+            await client.ConnectAsync(FirstIpPart + gateway.ToString(), Convert.ToInt32(port));
+
+            OnConnected?.Invoke(client);
+            Console.WriteLine("Connection");
+        }
+    }
+
+
+}
---
+++ Models/HistoryList.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using static AnimeList.Models.AnimeListConfig;
+
+namespace AnimeList.Models
+{
+    public class HistoryList
+    {
+        public List<HistoryModel> HList;
+
+        public HistoryList()
+        {
+            HList = new List<HistoryModel>();
+        }
+
+        public void SetHistoryList(List<HistoryModel> historyList)
+        {
+            HList = historyList;
+        }
+    }
+
+    public class HistoryModel
+    {
+        public List<IAction> HList { get; set; }
+
+        public Controls.TabControl Tab { get; set; }
+
+
+        public int CurrentPosition = 0;
+
+        public void AddAction(IAction action)
+        {
+            if (CurrentPosition < HList.Count)
+                HList.RemoveRange(CurrentPosition, HList.Count-CurrentPosition);
+            else CurrentPosition = HList.Count;
+            HList.Add(action);
+            CurrentPosition++;
+        }
+
+        public HistoryModel()
+        {
+
+        }
+
+        public void GoForward()
+        {
+            if (CurrentPosition + 1 > HList.Count)
+                return;
+            CurrentPosition++;
+            HList[CurrentPosition - 1].Do();
+        }
+
+        public void GoBack()
+        {
+            if (CurrentPosition < 1)
+                return;
+            HList[CurrentPosition - 1].Undo();
+            CurrentPosition--;
+        }
+    }
+
+    public interface IAction
+    {
+        public AnimeList AnimeList { get; set; }
+
+        abstract void Do();
+        abstract void Undo();
+    }
+
+    public class AddAction : IAction
+    {
+        public AnimeModel AModel { get; set; }
+        public AnimeList AnimeList { get; set; }
+     
+
+        public AddAction()
+        {
+
+        }
+        public void Do()
+        {
+            AnimeList.AddModel(AModel, true);
+        }
+
+        public void Undo()
+        {
+            AnimeList.RemoveModel(AModel.Place, true);
+        }
+    }
+
+    public class RemoveAction : IAction
+    {
+        public AnimeModel AModel { get; set; }
+        public AnimeList AnimeList { get; set; }
+
+
+        public RemoveAction()
+        {
+
+        }
+        public void Do()
+        {
+            AnimeList.RemoveModel(AModel.Place, true);
+        }
+
+        public void Undo()
+        {
+            AnimeList.AddModel(AModel, true);
+        }
+    }
+
+
+    public class EditAction : IAction
+    {
+        public AnimeModel AModel { get; set; }
+        public AnimeList AnimeList { get; set; }
+
+        public int PlaceA { get; set; }
+        public int PlaceB { get; set; }
+        public string NameA { get; set; }
+        public string NameB { get; set; }
+        public AnimeModelStatus WatchingA { get; set; }
+        public AnimeModelStatus WatchingB { get; set; }
+
+        public EditAction()
+        {
+
+        }
+        public void Do()
+        {
+            AnimeList.RemoveModel(PlaceA, true);
+            AModel.Place = PlaceB;
+            AModel.Name = NameB;
+            AModel.Status = WatchingB;
+            AnimeList.AddModel(AModel, true);
+        }
+
+        public void Undo()
+        {
+            AnimeList.RemoveModel(PlaceB, true);
+            AModel.Place = PlaceA;
+            AModel.Name = NameA;
+            AModel.Status = WatchingA;
+            AnimeList.AddModel(AModel, true);
+        }
+    }
+
+    public class WiFiAction : IAction
+    {
+        public AnimeList AnimeList { get; set; }
+        public AnimeList AnimeListBefore { get; set; }
+
+        public void Do()
+        {
+            AnimeListBefore.AL = AnimeList.AL;
+            AnimeListBefore.SaveChanges();
+        }
+
+        public void Undo()
+        {
+            AnimeList.AL = AnimeListBefore.AL;
+            AnimeList.SaveChanges();
+        }
+    }
+}
